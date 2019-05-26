3dc7c8b41aab19205330dc360dd1e44da64946ad
Ali <syed.hussain@qantas.com.au>
2019-05-26 11:26:40 +1000

    created the models

--- chatbotapp/chatapp/models.py
+++ chatbotapp/chatapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    profile = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
